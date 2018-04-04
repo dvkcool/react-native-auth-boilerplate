d28cb608dd92926a2722aa44952c01481cfc5d1c
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-04-04 20:32:07 +0530

    Adding a different function for login without hasura api

--- vanilla/README.md
+++ vanilla/README.md
@@ -355,7 +355,7 @@ const config = {
 };
 ```
 
-3. Comment out ```vanilla/auth/components/google/actions.js:30:1 - :47:12``` and implement your own method to handle auth token.
+3. Implement your own implementation of handling the "auth_token" in the `tryGooglelogin` function in ``auth/components/google/actions.js``.
 
 ### Demo
 #### Google Login
--- vanilla/auth/components/google/actions.js
+++ vanilla/auth/components/google/actions.js
@@ -3,7 +3,44 @@ import {storeSession} from '../../actions';
 import {clusterName} from '../../../Hasura';
 
 import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
-
+ trygooglelogin = async(user)=>{
+   const hasuraAuthUrl = `https://auth.${clusterName}.hasura-app.io/v1/signup`;
+   const options = {
+     "method": "POST",
+     "headers": {
+       "Content-Type": "application/json"
+     },
+     "body": JSON.stringify({
+       "provider": "google",
+       "data": {
+         "access_token": user.accessToken
+       }
+     })
+   };
+   try {
+     fetch(hasuraAuthUrl, options)
+     .then((res) => res.json())
+     .then((respObj)=>{
+       respObj['success'] = true;
+       respObj['google_profile_info'] = user;
+       storeSession({
+         id: respObj.hasura_id,
+         token: respObj.auth_token,
+         googleInfo: respObj.google_profile_info,
+         type: "google"
+       });
+       loginCallback({
+         id: respObj.hasura_id,
+         token: respObj.auth_token,
+         googleInfo: respObj.google_profile_info,
+         type: "google"
+       });
+     });
+   } catch (e) {
+   console.log(e);
+   return e;
+   }
+ }
 const handleGoogleAuth = async(androidClientId, iosClientid, loginCallback, startLoadingIndicator, stopLoadingIndicator) => {
   GoogleSignin.hasPlayServices({ autoResolve: true });
   GoogleSignin.configure({
@@ -13,42 +50,7 @@ const handleGoogleAuth = async(androidClientId, iosClientid, loginCallback, star
   .then(() => {
     GoogleSignin.signIn()
     .then((user) => {
-      const hasuraAuthUrl = `https://auth.${clusterName}.hasura-app.io/v1/signup`;
-      const options = {
-        "method": "POST",
-        "headers": {
-          "Content-Type": "application/json"
-        },
-        "body": JSON.stringify({
-          "provider": "google",
-          "data": {
-            "access_token": user.accessToken
-          }
-        })
-      };
-      try {
-        fetch(hasuraAuthUrl, options)
-        .then((res) => res.json())
-        .then((respObj)=>{
-          respObj['success'] = true;
-          respObj['google_profile_info'] = user;
-          storeSession({
-            id: respObj.hasura_id,
-            token: respObj.auth_token,
-            googleInfo: respObj.google_profile_info,
-            type: "google"
-          });
-          loginCallback({
-            id: respObj.hasura_id,
-            token: respObj.auth_token,
-            googleInfo: respObj.google_profile_info,
-            type: "google"
-          });
-        });
-      } catch (e) {
-      console.log(e);
-      return e;
-      }
+      trygooglelogin(user);
     })
     .catch((err) => {
     console.log('WRONG SIGNIN', err);
